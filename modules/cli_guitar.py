3c4e848a6798ae1ec1bb4c6ff52ebeac04cef8a6
Daniel Cruz <dftdcruz@gmail.com>
2020-02-18 01:55:52 -0300

    Criada funcao metronomo

--- modules/cli_guitar.py
+++ modules/cli_guitar.py
@@ -4,6 +4,10 @@
 
 import os
 import itertools
+import time
+
+from collections import OrderedDict
+from cli_tools import limpar_tela, amarelo, vermelho
 
 C = (
       (None, 'Dó', 'Mi', 'Sol', 'Dó', 'Mi'),
@@ -60,11 +64,140 @@ def print_cadence(list_of_cords, list_of_dedilhados):
                 tab = print_pima(cord, tab)
 
 
+def tempo(andamento):
+    a = int(andamento)
+    minima = (60/a * 1.0) * 2
+    seminima = (60/a * 1.0) / 1
+    colcheia = (60/a * 1.0) / 2
+    semicolcheia = (60/a * 1.0) / 4
+    return minima, seminima, colcheia, semicolcheia
+
+
+def compasso(proporcao, andamento):
+    
+    minima, seminima, colcheia, semicolcheia = tempo(andamento)
+
+    tic_nums = int(proporcao.split(':')[0])
+    tempo_referencia = int(proporcao.split(':')[1])
+
+    if tempo_referencia == 2:
+        tempo_compasso = tic_nums * minima
+
+    elif tempo_referencia == 4:
+        tempo_compasso = tic_nums * seminima
+
+    elif tempo_referencia == 8:
+        tempo_compasso = tic_nums * colcheia
+
+    elif tempo_referencia == 16:
+        tempo_compasso = tic_nums * semicolcheia
+
+
+    return tic_nums, tempo_compasso
+    
+    
+def metronomo(proporcao, andamento, notas):
+
+    tamanho_musica = len(notas.keys())
+
+    limpar_tela()
+    tic_nums, tempo_compasso = compasso(proporcao, andamento)
+    tempo_step = tempo_compasso/tic_nums
+    n = 0
+    m = 0
+    compassos = 1
+    mostrar_compasso = True
+    limpar_tela_ao_final = False
+
+    cd = int(tic_nums) * 2
+    print ('Iniciando m´usica em compasso {} e andamento {} em...'.format(proporcao, andamento))
+    while cd != 0:
+        print(cd)
+        cd -= 1
+        time.sleep(tempo_step)
+
+    print('')
+
+    contratempo_duracao = tempo_step/2
+
+    tp = True
+    concluir_musica = False
+    lirics_em_contratempo = False
+    escrever_lirics_em_contratempo = False
+    limpar_tela()
+
+    output_lines = {'head': '', 'body': ''}
+
+    while True:
+        if tp:
+            tp = False
+            if mostrar_compasso:
+                mostrar_compasso = False
+                output_lines['body'] += "Compasso:" + str(compassos) + os.linesep
+                #print("Compasso:", str(compassos))
+            n += 1
+            m += 1
+            nota_da_vez = '{}:{}'.format(compassos, m)
+            if notas.get(nota_da_vez):
+                nota = notas[nota_da_vez][0]
+                lirics = notas[nota_da_vez][1]
+                if len(notas[nota_da_vez]) > 2:
+                    lirics_em_contratempo = True
+                tamanho_musica -= 1
+            else:
+                nota = ''
+                lirics = False
+            
+            output_lines['head'] = '↓' + ' ' + os.linesep
+            output_lines['body'] += str(m) + ' ' + vermelho(str(nota))
+            if lirics:
+                if lirics_em_contratempo:
+                    escrever_lirics_em_contratempo = True
+                    lirics_em_contratempo = False
+                else:
+                    output_lines['body'] += os.linesep + amarelo(lirics)
+            output_lines['body'] += os.linesep
+
+            limpar_tela(msg=output_lines['head'] + output_lines['body'])
+            if n % tic_nums == 0:
+                m = 0
+                mostrar_compasso = True
+                compassos += 1
+                limpar_tela_ao_final = True
+                if tamanho_musica == 0:
+                    concluir_musica = True
+
+        else:
+            tp = True
+            output_lines['head'] = '↑' + os.linesep
+
+            if escrever_lirics_em_contratempo:
+                output_lines['body'] += '  ·' + amarelo(lirics) + os.linesep
+                escrever_lirics_em_contratempo = False
+            else:
+                output_lines['body'] += '  ·' + os.linesep 
+            limpar_tela(msg=output_lines['head'] + output_lines['body'])
+            if concluir_musica:
+                break
+            if limpar_tela_ao_final:
+                limpar_tela()
+                limpar_tela_ao_final = False
+                output_lines = {'head': '', 'body': ''}
+            
+
+        time.sleep(contratempo_duracao)
+            
+
+
+
+
+
+
 def print_pima(cord, tab):
     out = tab.copy()
     cad = 0
     for idx in range(0,6):
-        if cord[0[idx] == None:
+        if cord[0][idx] == None:
             pass
         else:
             if cad == 0:
@@ -72,6 +205,8 @@ def print_pima(cord, tab):
                 out[idx] = out[idx].ljust(6)
         
 
+
+'''
 
 E B C#m A
 
@@ -81,3 +216,4 @@ I IV III IV V I
 
 I IV III III IV I (trocar a cada duas)
 
+'''
